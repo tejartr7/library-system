d632a3598a5619a01863a9dd8d154f713aba6471
RAGHU TEJA REDDY SUREDDY <97605400+tejartr7@users.noreply.github.com>
2023-12-15 22:14:34 +0530

    Update Footer.jsx

--- client/src/pages/Footer.jsx
+++ client/src/pages/Footer.jsx
@@ -14,7 +14,6 @@ const Footer = () => {
   return (
     <div style={footerStyle} className='footer text-center mt-5 font-bold text-[#6469ff] text-xl uppercase'>
       <a href="https://twitter.com/raghu_rtr">Twitter </a>
-      <a href="https://www.linkedin.com/in/raghu-teja-reddy-sureddy-8b6721236/"> Linkedin </a>
       <a href="mailto:codworldrtr7@gmail.com"> Mail </a>
       <p>©All Rights Reserved</p>
     </div>
